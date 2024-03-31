24a330a0c136db0cad044988a52a0773561dc5a6
Aayan165 <ayanmahmood2006@gmail.com>
2024-03-31 16:07:16 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number from 1 to 5",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congractulations! Your guessed number is correct.");
+}
+else {
+    console.log("Oops! You guessed wrong number");
+}
